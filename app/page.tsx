f54ca561addd8481ec008af915398143fb43f93c
padel-reservas <alejandro.belinky@gmail.com>
2026-05-21 14:30:54 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2286,6 +2286,7 @@ export default function Page() {
             width: 280,
             height: 200,
             objectFit: 'cover',
+            objectPosition: 'top',
             borderRadius: 16,
             marginBottom: 16,
             border: '3px solid #fde047',
